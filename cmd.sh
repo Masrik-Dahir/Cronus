75cd6012f09c04cf97f619a57eafc909343e47f2
Masrik Dahir <dahirma@vcu.edu>
2022-04-15 22:38:47 -0400

    Create cmd.sh

---
+++ cmd.sh
@@ -0,0 +1,2 @@
+.\requirement.bat;
+python diagram.py 'ConceptMapFiles\CXLFiles\ComputerSecurity\Instructor\Module1IntroductionComputerSecurity\Lesson1ComputerSecurityOverview\CS_Overview.cmap.cxl' 'ConceptMapFiles\CXLFiles\ComputerSecurity\Student\Module1IntroductionComputerSecurity\ICSAnonymous6.cmap.cxl';
